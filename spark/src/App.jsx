a0503b51afec3de33c2ba6d8d9d0da9b7c473bf3
ink-projects <awlclemmons@gmail.com>
2025-03-07 16:04:35 -0500

    DropDown functionality added

--- spark/src/App.jsx
+++ spark/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header.jsx";
 import SearchBar from "./components/SearchBar.jsx";
+import DropDown from "./components/DropDown.jsx";
 
 function App() {
   return (
     <div>
         <Header />
         <SearchBar />
+        <DropDown />
     </div>
   );
 }
