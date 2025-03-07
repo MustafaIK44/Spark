3a6d54e3d3e28eaac3d468cda78103f2585d84d9
ink-projects <awlclemmons@gmail.com>
2025-03-07 16:22:28 -0500

    Hardcoded zipcodes into DropDown (for now)

--- spark/src/components/DropDown.jsx
+++ spark/src/components/DropDown.jsx
@@ -1,12 +1,20 @@
-
+/**
+ * DropDown functionality. 
+ * Zipcodes grabbed from this website: https://www.ciclt.net/sn/clt/capitolimpact/gw_ziplist.aspx?FIPS=51059
+ */
 export default function DropDown() {
     return (
         <div className="dropdown">
             <label tabIndex="0" className="btn m-1">Zip Codes</label>
             <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
-                <li><a>Item 2</a></li>
-                <li><a>Item 3</a></li>
+                <li><a>20151</a></li>
+                <li><a>20153</a></li>
+                <li><a>22031</a></li>
+                <li><a>22032</a></li>
+                <li><a>22033</a></li>
+                <li><a>22034</a></li>
+                <li><a>22035</a></li>
+                <li><a>22036</a></li>
             </ul>
         </div>
     );
