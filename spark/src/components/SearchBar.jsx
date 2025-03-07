75f36bc84dcc07a150abe2fa0049fa7755d65c84
scatteredEmbers <dziegen@gmu.edu>
2025-03-07 17:14:06 -0500

    messed w colors

--- spark/src/components/SearchBar.jsx
+++ spark/src/components/SearchBar.jsx
@@ -13,7 +13,7 @@ function AvailableItems({listID}) {
 
 export default function SearchBar() {
     return (
-        <label className="input primary-content">
+        <label className="input bg-base-300">
         <svg className="h-[2em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
         <input className="text-lg" type="search" required placeholder="Search for an item" list="available"/>
         <AvailableItems listID="available" />
