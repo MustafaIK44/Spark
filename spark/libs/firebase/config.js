4f947bdc170e0cabe524ca617539c95366558779
breezynotcheezy <87101443+breezynotcheezy@users.noreply.github.com>
2025-03-07 16:27:33 -0500

    base SDK

---
+++ spark/libs/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCz7psZeWnTvuNHwBppwB4AIz9-5jMgXwE",
+  authDomain: "spark-be1a0.firebaseapp.com",
+  projectId: "spark-be1a0",
+  storageBucket: "spark-be1a0.firebasestorage.app",
+  messagingSenderId: "137000992507",
+  appId: "1:137000992507:web:d715969b83eb7d7b9aba5e",
+  measurementId: "G-G2HSKCG6EW"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
