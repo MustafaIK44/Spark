3a17b91c013d35a2286e43a41029681015c2f88f
scatteredEmbers <dziegen@gmu.edu>
2025-03-09 16:26:21 -0400

    added more tests for searchbar

--- spark/__tests__/SearchBarTest.test.jsx
+++ spark/__tests__/SearchBarTest.test.jsx
@@ -3,10 +3,49 @@ import '@testing-library/jest-dom'
 import SearchBar from '../src/components/SearchBar.jsx';
 
 describe('SearchBar', () => {
-    render(<SearchBar/>)
-test("Search box renders successfully (with a list)", () => {
-    const element = screen.getByRole('combobox')
+    describe('has not been given an array', () => {
+        test("renders without a list", () => {
+            render(<SearchBar/>)
+            const element = screen.getByRole('searchbox')
+            expect(element).toBeInTheDocument()
+        })
 
-    expect(element).toBeInTheDocument()
-})
+        test("has a different placeholder", () => {
+            render(<SearchBar/>)
+            const element = screen.getByPlaceholderText('No items available')
+            expect(element).toBeInTheDocument()
+        })
+
+        test("is disabled", () => {
+            render(<SearchBar/>)
+            const element = screen.getByRole('searchbox')
+            expect(element).toBeDisabled()
+        })
+
+        test("should not be a combobox", () => {
+            render(<SearchBar/>)
+            const element = screen.queryByRole('combobox')
+            expect(element).not.toBeInTheDocument()
+        })
+    
+    })
+
+    describe('has been given an array.', () => {
+        const avail_items = ["Eggs", "Bread", "Milk", "Cereal", "Chicken", "Beef", "Apples", "Paper towels", "Toilet paper"];
+        test("Search box renders with a list directly given to it", () => {
+            render( <SearchBar items={["Eggs", "Bread", "Milk"]}/> )
+            const element = screen.getByRole('combobox')
+            expect(element).toBeInTheDocument()
+        })
+        test("Search box renders with a list indirectly given to it", () => {
+            render( <SearchBar items={avail_items}/> )
+            const element = screen.getByRole('combobox')
+            expect(element).toBeInTheDocument()
+        })
+        test("has a different placeholder", () => {
+            render(<SearchBar items={avail_items}/>)
+            const element = screen.getByPlaceholderText('Search')
+            expect(element).toBeInTheDocument()
+        })
+    })
 })
--- spark/src/components/SearchBar.jsx
+++ spark/src/components/SearchBar.jsx
@@ -1,24 +1,47 @@
-{/* this is for testing purposes */}
-const avail_items = ["Eggs", "Bread", "Milk", "Cereal", "Chicken", "Beef", "Apples", "Bananas", "Rice", "Spam", "Paper towels", "Toilet paper"];
-
+/**
+ * Helper function. Turns items from array into a option for the datalist.
+ * Each option should have a clear index so it renders properly.
+ * Returns the finalized datalist for the autocomplete/suggestions.
+ * @param listID is the id for the datalist (will always be available items).
+ * @param list is the array that must be parsed into options, and then put into the datalist.
+ * @returns the finalized datalist.
+ */
 function AvailableItems({listID, list}) {
     return (
         <datalist id={listID}>
-            {avail_items.map(
+            {list.map(
                 (item, index) => (<option value={item} key={index}> </option>))
             }
         </datalist>
     );
 }
 
-{/* */}
-export default function SearchBar() {   
+
+/**
+ * Allow users to provide a string of input on what item they would like to search.
+ * Uses the autocomplete built-in function to present suggestions to the user.
+ * If given an array of items (items) that is not empty, it will populate a datalist that will show as suggestions.
+ * Otherwise, the field should be disabled and not allow the user to input anything.
+ * @param items is an array.
+ * @returns the code to render the search bar.
+ */
+export default function SearchBar({items}) {   
+    {/* if there is no item list, then database is empty. */}
+    if (!items || items.length == 0) {
+        return (
+            <label className="input bg-base-300">
+            <svg className="h-[2em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+            <input className="text-lg" type="search" required placeholder="No items available" disabled/>
+            </label>
+        );
+    }
+
+    {/* if there are items, then allow user to type in the input field. Also, populate the datalist. */}
     return (
         <label className="input bg-base-300">
         <svg className="h-[2em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
-        <input className="text-lg" type="search" required placeholder="Search for an item" list="available"/>
-        <AvailableItems listID="available" list={avail_items} />
-        {/*data list should fetch data from the database when it's done*/}
+        <input role="combobox" className="text-lg" type="search" required placeholder="Search" list="available"/>
+        <AvailableItems listID="available" list={items} />
         </label>
     );
 }
