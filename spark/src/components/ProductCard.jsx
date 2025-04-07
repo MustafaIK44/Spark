d56eae7e399879123fca0877594cba14d5a5e8a9
MustafaIK44 <mkoc2@gmu.edu>
2025-04-07 16:37:12 -0400

    Some frontend changes.

--- spark/src/components/ProductCard.jsx
+++ spark/src/components/ProductCard.jsx
@@ -18,28 +18,28 @@ function ProductCard({productName, productStore, storeZipCode, productPrice, pro
     };
     
     return (
-        <div className="card bg-base-200 w-96 shadow-sm">
-    <figure className="px-10 pt-10">
-        <img
-        src={productImage}
-        width={500}
-        height={500}
-        alt="Stock image of Eggs"
-        className="rounded-xl" />
-    </figure>
-    <div className="card-body items-center text-center">
-        <h2 className="card-title">{productName} - {productPrice}</h2>
-        <p> {productStore}, {storeZipCode} </p>
-        <div className="card-actions">
-        <Button
-            onClick={handleAddClick}
-            text="Add to list"
-            className="btn btn-primary w-30"
-        />
+        <div className="card bg-base-200 w-72 shadow-sm p-2">
+            <figure className="px-10 pt-10">
+            <img
+                src={productImage}
+                width={500}
+                height={500}
+                alt="Stock image of Eggs"
+                className="rounded-xl " />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{productName} - {productPrice}</h2>
+                <p> {productStore}, {storeZipCode} </p>
+                <div className="card-actions">
+                <Button
+                    onClick={handleAddClick}
+                    text="Add to list"
+                    className="btn btn-primary w-30"
+                />
 
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
     );
 }
 
