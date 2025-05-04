97f91f1ab399b77e26949ec90919051ea35236bc
Fairy in a Bottle <awlclemmons@gmail.com>
2025-05-04 11:45:05 -0400

    edited jest config file

--- spark/jest.config.js
+++ spark/jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
-    },
-    moduleNameMapper: {
-      "\\.(css|scss|sass)$": "identity-obj-proxy"
-    }
-  }; 
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // <-- Add this line
+    "\\.(css|scss|sass)$": "identity-obj-proxy"
+  },
+  moduleDirectories: ['node_modules', 'src'], // <-- Optional: makes src/ imports easier
+};
